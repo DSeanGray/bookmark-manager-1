a0ba729bdf901d95a24b5545da655f4c388792dc
BenNoonan1991 <ben.noonan2@gmail.com>
2017-05-02 15:05:43 +0100

    Adds first feature test

---
+++ app.rb
@@ -0,0 +1,6 @@
+require 'sinatra/base'
+
+class App < Sinatra::Base
+  enable :sessions
+
+end
---
+++ spec/features/view_links_spec.rb
@@ -0,0 +1,15 @@
+# require 'spec_helper'
+# require 'rails_helper'
+# require 'rspec'
+#
+# RSpec.feature 'View links', :type => :feature do
+#   scenario 'view a list of links' do
+#     # Link.create(url: 'http://bbc.com', title: 'BBC')
+#     # visit '/links'
+#     # expect(page.status_code).to eq 200
+#     #
+#     # within 'ul#links' do
+#     # expect(page).to have_content 'BBC'
+#     #end
+#   end
+# end
