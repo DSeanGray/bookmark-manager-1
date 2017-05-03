33a6a5486ed99b2e69aa9bb485f5f4b123571d16
BenNoonan1991 <ben.noonan2@gmail.com>
2017-05-03 15:27:50 +0100

    Updated datamapper settings

--- lib/links.rb
+++ lib/links.rb
@@ -1,6 +1,6 @@
 require 'data_mapper'
 
-class Links 
+class Links
 	include DataMapper::Resource
 
 	property :id, Serial
@@ -9,6 +9,6 @@ class Links
 
 end
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup((:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
